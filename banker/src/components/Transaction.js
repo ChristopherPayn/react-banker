8a07c72778a6a1c79241d3d96a4fcf62788d21f4
ChristopherPayn <christopher.payn@hotmail.co.uk>
2023-03-25 17:00:03 +0000

    adds buttons to pay and be paid by the bank

--- banker/src/components/Transaction.js
+++ banker/src/components/Transaction.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import * as ds from '../DataStore';
+import { BsBank } from 'react-icons/bs';
 import styles from './styles/Transaction.module.css';
 import globalStyles from './styles/Global.module.css';
 
@@ -11,6 +12,7 @@ const Transaction = () => {
     const handleSubmit = e => {
         e.preventDefault();
         ds.performTransaction(foundFromPlayer, foundToPlayer, transactionAmount);
+        clearForm();
     };
 
     const handleChange = e => {
@@ -30,41 +32,79 @@ const Transaction = () => {
         }
     };
 
+    const populateBank = (target) => {
+        const targetInput = document.querySelector(`#account-number-${target}`);
+        targetInput.value = 'BANK';
+        if (target === 'from') {
+            setFoundFromPlayer('BANK');
+        } else {
+            setFoundToPlayer('BANK');
+        }
+    };
+
+    const clearForm = () => {
+        setFoundFromPlayer(null);
+        setFoundToPlayer(null);
+        setTransactionAmount(0);
+        document.querySelector('#account-number-from').value = '';
+        document.querySelector('#account-number-to').value = '';
+        document.querySelector('#transaction-amount').value = '';
+    };
+
     return (
         <div className={globalStyles.formContainer} onSubmit={handleSubmit}>
             <h1 className={`${globalStyles.formHeader} ${styles.header}`} id={styles.transactionsHeader}>Transactions</h1>
             <form className={globalStyles.transactionForm}>
                 <h2>From:</h2>
                 <label htmlFor='account-number-from'>Account number:</label>
-                <input
-                    id='account-number-from'
-                    type='text'
-                    name='accountNumberFrom'
-                    placeholder='Enter account number'
-                    onChange={handleChange}
-                    required
-                />
-                {foundFromPlayer && (
-                    <div>
-                        <span>Account belongs to: {foundFromPlayer.name}</span>
+                <div className={styles.transactionFormInputContainer}>
+                    <input
+                        className={styles.transactionFormInput}
+                        id='account-number-from'
+                        type='text'
+                        name='accountNumberFrom'
+                        placeholder='Enter account number'
+                        onChange={handleChange}
+                        required
+                    />
+                    <div
+                        className={styles.transactionFormBank}
+                        onClick={() => populateBank('from')}
+                    >
+                       <span className={styles.transactionFormBankIcon}><BsBank /></span>
+
                     </div>
-                )}
+                </div>
+                {(foundFromPlayer && (
+                    <div>
+                        <span>Account belongs to: {foundFromPlayer.name || 'the bank'}</span>
+                    </div>
+                )) || <div>Awaiting input and searching players...</div>}
                 <hr />
                 <h2>To:</h2>
                 <label htmlFor='account-number-to'>Account number:</label>
-                <input
-                    id='account-number-to'
-                    type='text'
-                    name='accountNumberTo'
-                    placeholder='Enter account number'
-                    onChange={handleChange}
-                    required
-                />
-                {foundToPlayer && (
-                    <div>
-                        <span>Account belongs to: {foundToPlayer.name}</span>
+                <div className={styles.transactionFormInputContainer}>
+                    <input
+                        className={styles.transactionFormInput}
+                        id='account-number-to'
+                        type='text'
+                        name='accountNumberTo'
+                        placeholder='Enter account number'
+                        onChange={handleChange}
+                        required
+                    />
+                    <div
+                        className={styles.transactionFormBank}
+                        onClick={() => populateBank('to')}
+                    >
+                        <span className={styles.transactionFormBankIcon}><BsBank /></span>
                     </div>
-                )}
+                </div>
+                {(foundToPlayer && (
+                    <div>
+                        <span>Account belongs to: {foundToPlayer.name || 'the bank'}</span>
+                    </div>
+                )) || <div>Awaiting input and searching players...</div>}
                 <hr />
                 <h2>Amount:</h2>
                 <label htmlFor='transaction-amount'>Amount to be paid:</label>
@@ -76,9 +116,10 @@ const Transaction = () => {
                     onChange={handleChange}
                     required
                 />
-                {foundFromPlayer && foundToPlayer && transactionAmount !== 0 &&
-                    <div>{foundFromPlayer?.name || ''} will pay {foundToPlayer?.name || ''} the amount of {transactionAmount}</div>
-                }
+                {(foundFromPlayer && foundToPlayer && transactionAmount !== 0 &&
+                    <div>{foundFromPlayer?.name || 'The bank'} will pay {foundToPlayer?.name || 'the bank'} the amount of {transactionAmount}</div>
+                ) || <div>Awaiting payment amount</div>}
+                <br />
                 <button type="submit">Confirm and pay</button>
             </form>
         </div>
